c431beffdc5dfac6753b02c095a7526b80a44d5d
wuguzi <puxiao.wh@alibaba-inc.com>
2016-11-04 23:51:41 +0800

    save

--- model/prefixInteger.js
+++
@@ -1,9 +0,0 @@
-/**
- *  author: puxiao
- *  time: 2016-10-05
- *  des: 自动补零
- */
-// num 数字，n 需要的位数
-module.exports = function prefixInteger(num, n) {
-    return (Array(n).join(0) + num).slice(-n);
-}
